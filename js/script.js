bd08013ded1d10bfe03451acde0b3e5a68190cbf
JesusChristianDev <jesus1999misu@gmail.com>
2025-06-04 12:03:27 +0200

    Documentación: comentarios explicativos en HTML, CSS y JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+// script.js
 // Mensaje de bienvenida en consola
-console.log("Hola Jesús, ¡tu landing ya está corriendo!");
+console.log("Hola Jesús, ¡mi landing ya está corriendo!");
 
 // script.js
 
 // Mostrar / ocultar menú en móvil
+// Este bloque gestiona el menú responsive para dispositivos móviles
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('nav-links-visible');
+    // Alterna la visibilidad del menú al hacer click en el botón hamburguesa
 });
